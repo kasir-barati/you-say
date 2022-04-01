929f89d492eaaf3ae6e26abd217fac48d6f3b52a
Kasir Barati <kasir.barati@gmail.com>
2022-04-01 10:18:40 +0430

    users: move files

--- src/packages/users/users.controller.ts
+++ src/packages/users/users.controller.ts
@@ -9,8 +9,8 @@ import {
 } from '@nestjs/common';
 
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/request/create-user.dto';
+import { UpdateUserDto } from './dto/request/update-user.dto';
 
 @Controller('users')
 export class UsersController {
--- src/packages/users/users.service.ts
+++ src/packages/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/request/create-user.dto';
+import { UpdateUserDto } from './dto/request/update-user.dto';
 
 @Injectable()
 export class UsersService {
