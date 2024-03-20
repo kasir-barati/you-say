e1a4484a09a1f2c5bd3846eaa08b4bfb6c66475b
Kasir Barati <kasir.barati@gmail.com>
2024-03-20 04:44:38 +0100

    fix: wip -- fusionauth host address

--- apps/backend-e2e/setup-env.ts
+++ apps/backend-e2e/setup-env.ts
@@ -3,8 +3,21 @@ import axios, { AxiosError } from 'axios';
 axios.interceptors.response.use(
   (response) => response,
   (error: AxiosError) => {
-    console.dir(error.request, { depth: null });
-    console.dir(error.response, { depth: null });
+    const request = {
+      method: error.request?.method,
+      path: error.request?.path,
+    };
+    const response = {
+      statusCode: error.response?.status,
+      requestConfig: {
+        url: error.response?.config?.url,
+        method: error.response?.config?.method,
+        data: error.response?.config?.data,
+      },
+      data: error.response?.data,
+    };
+    console.dir({ request }, { depth: null });
+    console.dir({ response }, { depth: null });
     return Promise.reject(error);
   },
 );
