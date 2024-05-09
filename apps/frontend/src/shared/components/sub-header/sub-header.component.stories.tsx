34607193b43b0b190e4a32c612e9d95a0844abf8
Kasir Barati <kasir.barati@gmail.com>
2024-05-09 13:41:29 +0200

    fix: failing storybook test + missing notification in mock provider

--- apps/frontend/src/shared/components/sub-header/sub-header.component.stories.tsx
+++ apps/frontend/src/shared/components/sub-header/sub-header.component.stories.tsx
@@ -1,10 +1,28 @@
 import { Meta, StoryObj } from '@storybook/react';
+import { HttpResponse, http } from 'msw';
+import { MockProvider } from '../../test-utils/mock-provider.test-util';
 import { SubHeader } from './sub-header.component';
 
 type Story = StoryObj<typeof SubHeader>;
 
 export default {
   component: SubHeader,
+  render() {
+    return (
+      <MockProvider>
+        <SubHeader />
+      </MockProvider>
+    );
+  },
+  parameters: {
+    msw: {
+      handlers: [
+        http.post('*/newsletter-subscription', () =>
+          HttpResponse.json(),
+        ),
+      ],
+    },
+  },
 } satisfies Meta<typeof SubHeader>;
 
 export const Default: Story = {};
--- apps/frontend/src/shared/test-utils/mock-provider.test-util.tsx
+++ apps/frontend/src/shared/test-utils/mock-provider.test-util.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from 'react';
 import { Provider } from 'react-redux';
+import { Notification } from '../components/notification/notification.component';
 import { PreloadedState, createStore } from '../store/create.store';
 
 interface MockProviderProps {
@@ -12,5 +13,10 @@ export function MockProvider({
 }: Readonly<PropsWithChildren<MockProviderProps>>) {
   const store = createStore(initialState);
 
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      {children}
+      <Notification />
+    </Provider>
+  );
 }
