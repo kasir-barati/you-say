aea61a68e1d9898b29dd6cb519867582522e27e5
Kasir Barati <kasir.barati@gmail.com>
2024-10-30 21:51:38 +0100

    chore: add cache example for nx

---
+++ .github/docs/monorepo/cache.js
@@ -0,0 +1,17 @@
+// libs/lib1/project.json
+{
+  "targets": {
+    "test": {
+      "cache": true
+    }
+  }
+}
+
+// nx.json
+{
+  "targetDefaults": {
+    "build": {
+      "cache": true
+    }
+  }
+}
