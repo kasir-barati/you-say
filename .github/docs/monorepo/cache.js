5a15e2bd131f8bfbb79e022a8b08ae5b5c2dd62d
Kasir Barati <kasir.barati@gmail.com>
2024-10-30 22:06:09 +0100

    chore: add 1 more cache example for nx

--- .github/docs/monorepo/cache.js
+++ .github/docs/monorepo/cache.js
@@ -6,12 +6,28 @@
     }
   }
 }
+{
+  "targetDefaults": {
+    "build": {
+      "inputs": ["{projectRoot}/**/*", "!{projectRoot}/**/*.md"],
+      "outputs": ["{workspaceRoot}/dist/{projectName}"]
+    }
+  }
+}
 
 // nx.json
 {
   "targetDefaults": {
     "build": {
       "cache": true
+    }
+  }
+}
+{
+  "targetDefaults": {
+    "build": {
+      "inputs": ["{projectRoot}/**/*", "!{projectRoot}/**/*.md"],
+      "outputs": ["{workspaceRoot}/dist/{projectName}"]
     }
   }
 }
